e484cb8d24f977488ed090a1e1b96a81868ea945
AKASUsan <kasubaranime@gmail.com>
2026-03-02 02:18:11 +0700

    open web

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const session = require("express-session");
+
+const router = require("./routers/myrouter");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "mysecretkey",
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
+
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || null;
+  next();
+});
+
+app.use(router);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(8080, () => {
+  console.log("Starting server at port: 8080");
+});
