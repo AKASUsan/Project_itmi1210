1981f72fbfbf3e98ac68161b05198f2c6a527863
AKASUsan <kasubaranime@gmail.com>
2026-03-03 21:53:52 +0700

    add const Member in myrouter and add data table in mongodb serve

--- routers/myrouter.js
+++ routers/myrouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const connectDB = require("../config/db")
+const Member = require("../models/members");
 
 router.get("/", (req, res) => {
   res.render("index");
