d4a2aad4fe0cc91758cebb2c6f1aaaa61d4d0f13
AKASUsan <kasubaranime@gmail.com>
2026-03-06 15:50:57 +0700

    edit show data by mongodb

--- routers/myrouter.js
+++ routers/myrouter.js
@@ -3,9 +3,19 @@ const router = express.Router();
 
 const connectDB = require("../config/db")
 const Member = require("../models/members");
+const Product = require("../models/products")
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/",  async (req, res) => {
+  const title = "Home";
+  try{
+    const products = await Product.find();
+    res.render("index",{
+      title: title,
+      products: products
+    });
+  }catch (err) {
+    res.status(500).json({message:"ServerError", error: err.message })
+  }
 });
 
 module.exports = router;
