d37f8bb44b770c54a649873963ddcf3b587b2162
AKASUsan <kasubaranime@gmail.com>
2026-03-21 16:46:04 +0700

    profile page & Edit profile

--- routers/routerAuth.js
+++ routers/routerAuth.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const auth = express.Router();
 const bcrypt = require("bcryptjs");
-
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 const Member = require("../models/members");
 const Employee = require("../models/employees");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const currentUser = req.session?.user || req.user;
+
+    const folder =
+      currentUser &&
+      (currentUser.role === "admin" || currentUser.role === "staff")
+        ? "./public/images/employeeProfile/"
+        : "./public/images/profile/";
+
+    cb(null, folder);
+  },
+  filename: function (req, file, cb) {
+    const fileExt = file.originalname.split(".").pop();
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, "profile-" + uniqueSuffix + "." + fileExt);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 auth.get("/register", (req, res) => {
   res.render("auth/register");
@@ -19,6 +42,64 @@ auth.get("/logout", (req, res) => {
   req.session.destroy(() => {
     res.redirect("/signin");
   });
+});
+
+auth.get("/profile", async (req, res) => {
+  if (!req.session.user) {
+    return res.redirect("/signin");
+  }
+  try {
+    const title = "Profile";
+    let user =
+      (await Member.findById(req.session.user._id)) ||
+      (await Employee.findById(req.session.user._id));
+
+    if (!user) {
+      req.session.destroy();
+      return res.redirect("/signin");
+    }
+    res.render("auth/profile", {
+      title: title,
+      user: user,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server Error", error: err.message });
+  }
+});
+
+auth.post('/updateprofile', upload.single('image'), async (req, res) => {
+    try {
+        const currentUser = req.session?.user || req.user;
+        
+        if (!currentUser) {
+            return res.status(401).send('Unauthorized');
+        }
+
+        const userId = currentUser._id; 
+        const { name, phone, address } = req.body;
+        let updateData = { name, phone, address };
+
+        if (req.file) {
+            updateData.image = req.file.filename; 
+        }
+
+        let TargetModel = Member;
+        if (currentUser.role === 'admin' || currentUser.role === 'staff') {
+            TargetModel = Employee;
+        }
+
+       const updatedUser = await TargetModel.findByIdAndUpdate(userId, updateData, { returnDocument: 'after' });
+
+        if (req.session && req.session.user) {
+            req.session.user = updatedUser;
+        }
+
+        res.redirect('/profile'); 
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 auth.post("/register", async (req, res) => {
