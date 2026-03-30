8d77a2f44ba0ac0c06fb50c8d06adbcdcbe2a0c2
AKASUsan <kasubaranime@gmail.com>
2026-03-30 23:48:04 +0700

    Edit route

--- models/promotion.js
+++ models/promotion.js
@@ -6,7 +6,13 @@ const PromotionSchema = new mongoose.Schema({
     imageFilename: { type: String, required: true },
     isActive: { type: Boolean, default: true },
     startDate: { type: Date },
-    endDate: { type: Date }
+    endDate: { type: Date },
+    applicableItem: { type: String },    
+    originalPrice: { type: Number },       
+    discountPercent: { type: Number },   
+    discountedPrice: { type: Number }
+    
+
 }, { timestamps: true });
 
 module.exports = mongoose.model("Promotion", PromotionSchema);
